77082ed67aa4188a8cbbf84216298da8f798659a
williamthing <williamthing@gmail.com>
2015-04-30 10:55:01 -0700

    finished store with 6 items and test bench

--- Department Store/Shop/storeitems.sv
+++ Department Store/Shop/storeitems.sv
@@ -42,3 +42,128 @@ default: leds = 7'bX;
 // uses endcase because of case statement
 endcase
 endmodule
+
+module mystore (item, out0, out1, out2, out3, out4, out5);
+input [2:0] item;
+output reg [6:0] out0, out1, out2, out3, out4, out5;
+
+always @(*)
+case (item)
+
+// ring
+3'b000: 
+begin
+out0 = 7'b1111111;
+out1 = 7'b1111111;
+out2 = 7'b0010000;
+out3 = 7'b1001000;
+out4 = 7'b1001111;
+out5 = 7'b1001110;
+end
+
+// ball
+3'b100:
+begin
+out0 = 7'b1111111;
+out1 = 7'b1111111;
+out2 = 7'b1000111;
+out3 = 7'b1000111;
+out4 = 7'b0001000;
+out5 = 7'b0000000;
+end
+
+// boob
+3'b110:
+begin
+out0 = 7'b1111111;
+out1 = 7'b1111111;
+out2 = 7'b0000000;
+out3 = 7'b1000000;
+out4 = 7'b1000000;
+out5 = 7'b0000000;
+end
+
+// glasses
+3'b001:
+begin
+out0 = 7'b1111111;
+out1 = 7'b0111011;
+out2 = 7'b0111011;
+out3 = 7'b1000000;
+out4 = 7'b0111111;
+out5 = 7'b1000000;
+end
+
+// PC
+3'b101:
+begin
+out0 = 7'b1111111;
+out1 = 7'b1111111;
+out2 = 7'b1111111;
+out3 = 7'b1111111;
+out4 = 7'b1000110;
+out5 = 7'b0001100;
+end
+
+// Chair
+3'b011:
+begin
+out0 = 7'b1111111;
+out1 = 7'b1001110;
+out2 = 7'b1001111;
+out3 = 7'b0001000;
+out4 = 7'b0001001;
+out5 = 7'b1000110;
+end
+
+
+default:
+begin
+out0 = 7'bX;
+out1 = 7'bX;
+out2 = 7'bX;
+out3 = 7'bX;
+out4 = 7'bX;
+out5 = 7'bX;
+end
+
+endcase
+endmodule
+
+// lab3 integration
+module labint (lit, swatch);
+output [1:0] lit;
+input [3:0] swatch;
+
+wire A, B, C, X;
+
+// Stolen or Not
+not(A, swatch[0]);
+nor(B, A, swatch[1], swatch[3]);
+nor(C, swatch[0], swatch[2], swatch[3]);
+or(lit[0], B, C);
+
+// Discounted
+and(X, swatch[0], swatch[2]);
+or(lit[1], swatch[1], X);
+
+endmodule
+
+// test bench
+module Lab3_testbench();
+wire [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+wire [1:0] LEDR;
+reg [7:0] SW;
+Lab3 dut (.HEX0, .HEX1, .HEX2, .HEX3, .HEX4, .HEX5, .LEDR,
+.SW);
+// Try all combinations of inputs.
+integer i;
+initial begin
+// set these switches to not being used
+
+
+for(i = 0; i < 256; i++) begin
+SW[7:0] = i; #10;
+end
+end
+endmodule
