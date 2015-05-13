4670f86fe877553b69277cef34725adc0d3ca7de
William Thing <william@cloudboltsoftware.com>
2015-05-12 23:43:47 -0700

    Update tugofwar.sv

--- Tug of War/tugofwar.sv
+++ Tug of War/tugofwar.sv
@@ -1,3 +1,17 @@
+module Lab3 (CLOCK_50,SW, LEDR,KEY); 
+	input CLOCK_50; 
+	input SW[9];
+	input [3:0] KEY;
+	output reg [9:1] LEDR;
+	output reg [6:0] HEX[0];
+	parameter whichClock = 23; 	
+
+	
+	
+	clock_divider cdiv (CLOCK_50, clk);   
+endmodule 
+
+
 // User Input
 module button(Clock, pressed, set);
 	input Clock;
