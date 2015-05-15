3710300738c90a6034754562a396c05ee6bbc861
William Thing <william@cloudboltsoftware.com>
2015-05-14 20:07:32 -0700

    Update tugofwar.sv

--- Tug of War/tugofwar.sv
+++ Tug of War/tugofwar.sv
@@ -1,4 +1,5 @@
 // William Thing
+// Lab 6: Tug of War
 
 module Lab3 (CLOCK_50, SW, LEDR, KEY, HEX0); 
 	input CLOCK_50; 
@@ -8,15 +9,14 @@ module Lab3 (CLOCK_50, SW, LEDR, KEY, HEX0);
 	output reg [6:0] HEX0;	
 	assign LEDR[0] = 1'b0;
 	wire right,left;
-	parameter whichClock = 23;
-	
-	button b1 (.Clock(CLOCK_50) , .pressed(~KEY[3]), .set(left));
-	button b2 (.Clock(CLOCK_50) , .pressed(~KEY[0]), .set(right));
 	
 	
-	display disp (.Clock(CLOCK_50), .Reset(SW[9]), .L(left), .R(right), .NL(LEDR[9]), .NR(LEDR[1]), .lightOn(HEX0));
+	// buttons for the two different players 
+	button player2 (.Clock(CLOCK_50) , .Reset(SW[9]), .pressed(~KEY[3]), .set(left));
+	button player1 (.Clock(CLOCK_50) , .Reset(SW[9]), .pressed(~KEY[0]), .set(right));
 	
 	
+	// we have normal lights and 1x center light
 	normalLight n1(.Clock(CLOCK_50), .Reset(SW[9]), .L(left), .R(right), .NL(LEDR[2]), .NR(1'b0), .lightOn(LEDR[1]));
 	normalLight n2(.Clock(CLOCK_50), .Reset(SW[9]), .L(left), .R(right), .NL(LEDR[3]), .NR(LEDR[1]), .lightOn(LEDR[2]));
 	normalLight n3(.Clock(CLOCK_50), .Reset(SW[9]), .L(left), .R(right), .NL(LEDR[4]), .NR(LEDR[2]), .lightOn(LEDR[3]));
@@ -27,6 +27,9 @@ module Lab3 (CLOCK_50, SW, LEDR, KEY, HEX0);
 	normalLight n8(.Clock(CLOCK_50), .Reset(SW[9]), .L(left), .R(right), .NL(LEDR[9]), .NR(LEDR[7]), .lightOn(LEDR[8]));
 	normalLight n9(.Clock(CLOCK_50), .Reset(SW[9]), .L(left), .R(right), .NL(1'b0),    .NR(LEDR[8]), .lightOn(LEDR[9]));
 	
+	
+	// Hexdisplay for winner for Tug of War Game
+	hexdisplay disp (.Clock(CLOCK_50), .Reset(SW[9]), .L(left), .R(right), .NL(LEDR[9]), .NR(LEDR[1]), .lightOn(HEX0));
 	   
 endmodule 
  
@@ -98,6 +101,7 @@ parameter off = 1'b0, on = 1'b1;
 // when lightOn is true, the normal light should be on.
 output reg lightOn;
 
+// wrong logic
 // while
 /*
 always @(*)
@@ -109,6 +113,7 @@ case(PS)
 	default: NS = 1'bx;	
 endcase
 */
+
 // while
 always @(*)
 case(PS)
@@ -139,24 +144,42 @@ endmodule
 
 
 
-// User Input
-module button(Clock, pressed, set);
-	input Clock;
+// User Input for bottons
+module button(Clock, Reset, pressed, set);
+	input Clock, Reset;
 	input pressed;
 	output reg set;
-	reg PS, NS;
+	reg [1:0] PS, NS;
+	parameter [1:0] on = 2'b00, hold = 2'b01, off = 2'b10;
 	
 	always @(*)
-		NS = pressed;
-	always @(posedge Clock)
-		PS <= NS;
+	case(PS)
+		on:	if (pressed) NS = hold;
+				else NS = off;
+		hold:	if (pressed) NS = hold;
+				else NS = off;
+		off: 	if (pressed) NS = on;
+				else NS = off;
+		default: NS = 2'bxx;
+	endcase
 	
-	assign set = (~PS & NS);
+	always @(*)
+	case(PS)
+		on: set = 1;
+		hold: set = 0;
+		off: set = 0;
+		default: set = 1'bx;
+	endcase
+		
+	always @(posedge Clock)
+		if (Reset) PS <= off;
+		else PS <= NS;
 	
 endmodule
 
 
-module display(Clock, Reset, L, R, NL, NR, lightOn);
+
+module hexdisplay(Clock, Reset, L, R, NL, NR, lightOn);
 	input Clock, Reset;
 	input L, R, NL, NR;
 	reg [6:0] PS, NS;
@@ -184,6 +207,240 @@ module display(Clock, Reset, L, R, NL, NR, lightOn);
 	
 
 endmodule
+
+
+module Lab3_testbench(); //CLOCK_50, KEY, SW, LEDR, HEX0
+	reg CLOCK_50;
+	reg [9:0] SW;
+	reg [3:0] KEY;
+	reg [9:0] LEDR;
+	reg [6:0] HEX0;
+	
+
+	
+	Lab3 dut (CLOCK_50, SW, LEDR, KEY, HEX0);
+	
+
+	initial CLOCK_50 = 1;
+	initial KEY[3:0] = 4'b1111;
+	parameter CLOCK_PERIOD = 100;
+	
+	always begin
+			#(CLOCK_PERIOD/2);
+		CLOCK_50 = ~CLOCK_50;
+	end
+
+	
+	
+	initial begin
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+		// reset is off								
+		SW[9] <= 0; 		@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+		// reset is on								
+		SW[9] <= 1;			@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+		
+		// reset is off
+		SW[9] <= 0;
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+										
+		KEY[0] <= 0; 			
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+		KEY[0] <= 1;
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+								KEY[0] <= 0; 				@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+		KEY[0] <= 1;		@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+								KEY[0] <= 0; 				@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+		KEY[0] <= 1;		@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+								KEY[0] <= 0; 				@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								
+		KEY[0] <= 1;		@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+								KEY[0] <= 0; 				@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+		KEY[0] <= 1;		@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+								KEY[0] <= 0; 				@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+										
+		KEY[0] <= 1;		@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+
+		// reset is on								
+		SW[9] <= 1;			@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+		
+		// reset is off
+		SW[9] <= 0;
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);
+								@(posedge CLOCK_50);								
+		
+
+		
+		$stop;
+	end
+endmodule
+
+
+module centerLight_testbench();
+
+	reg Clk, Reset;
+	
+	wire out;
+	wire [3:0] KEY;
+	wire [6:0] HEX;
+	wire [9:0] LEDR;
+
+	reg [9:0] SW;	
+	reg NL, NR, L, R;
+	centerLight dut(.Clock(Clk), .Reset(Reset), .L(L), .R(R), .NL(NL), .NR(NR), .lightOn(LEDR[5]));
+
+
+	parameter CLOCK_PERIOD=100;
+
+	initial Clk = 1;
+	always begin
+			#(CLOCK_PERIOD/2);
+		Clk = ~Clk;
+	end
+	// Set up the inputs to the design. Each line is a clock cycle.
+	initial begin
+
+		Reset <= 1; @(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						
+						
+		Reset <= 0;	@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						
+						
+		L <= 1;		@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						
+						
+		NR <= 1;		@(posedge Clk); 
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						
+						
+		NR <= 0;		@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);	
+						@(posedge Clk);
+						@(posedge Clk);
+						
+						
+		L <= 0;		@(posedge Clk); 
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+	
+	
+		R <= 1;		@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						
+						
+		NL <= 1; 	@(posedge Clk); 
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						
+						
+		NL <= 0;		@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+						
+						
+		R <= 0;		@(posedge Clk);
+						@(posedge Clk);
+						@(posedge Clk);
+					   @(posedge Clk);
+										
+		$stop;
+	end
+endmodule
+
+
 
 /*
 module Lab3_testbench();
